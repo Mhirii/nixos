5f24ab5ec49a5480271f234bf40ef341de478d57
Mhirii <ahmedmhiri218@gmail.com>
2024-08-16 09:00:08 +0100

    cleanup

--- home-manager/programs/hyprland/default.nix
+++ home-manager/programs/hyprland/default.nix
@@ -1,18 +1,8 @@
 { pkgs, lib, ... }:
-let
-  flake_dir = "/home/mhiri/flake";
-in
 {
-
   wayland.windowManager.hyprland = {
     enable = true;
   };
-
-  # home.file.launch = {
-  #   target = ".config/hypr/scripts/launch.fish";
-  #   source = "${flake_dir}/home-manager/assets/launch.fish";
-  #   executable = true;
-  # };
 
   imports = [
     ./scripts
