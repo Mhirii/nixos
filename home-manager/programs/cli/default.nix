26170697d51887cb0b3bedfa5280ce70d4876dd1
Mhirii <ahmedmhiri218@gmail.com>
2024-08-27 03:40:42 +0100

    cli

--- home-manager/programs/cli/default.nix
+++ home-manager/programs/cli/default.nix
@@ -6,5 +6,17 @@
     ./git.nix
     ./tmux.nix
     ./zoxide.nix
+    ./cava.nix
   ];
+  programs.bat.enable = true;
+  programs.fd.enable = true;
+  programs.fd.hidden = true;
+  programs.fd.ignores = [ ".git/*" "node_modules/*" "dist/*" "tmp/*" ];
+  programs.fzf.enable = true;
+  programs.fzf.tmux.enableShellIntegration = true;
+  programs.jq.enable = true;
+  programs.lazygit.enable = true;
+  programs.man.enable = true;
+  programs.navi.enable = true;
+  programs.ripgrep.enable = true;
 }
