b6c337e71c34272410614932d3e6d095df94fa8c
Mhirii <ahmedmhiri218@gmail.com>
2024-08-29 16:18:20 +0100

    monitors setup

--- home-manager/programs/hyprland/monitors.nix
+++ home-manager/programs/hyprland/monitors.nix
@@ -1,14 +1,21 @@
-{ ... }: {
-  wayland.windowManager.hyprland.settings = {
-
-
-    #monitor= eDP-1, 1920x1080@60.00000, 0x0, 1
-    monitor = [
+{ profile, ... }:
+let
+  monitor = {
+    laptop = [
+      "Unknown-1, disable"
+      "eDP-1, preferred, auto, 1.00 "
+      "HDMI-A-2 , 1920x1080@60, -1920x0, 1"
+    ];
+    desktop = [
       "Unknown-1, disable"
       ", preferred, auto, 1.00 "
-      # "monitor= , preferred, 1920x0, 1.00"
-      # "HDMI-A-2 , 1920x1080@60, -1920x0, 1"
-      # "HDMI-A-2 , 1920x1080@60, 0x0, 1"
     ];
+  };
+in
+{
+  wayland.windowManager.hyprland.settings = {
+
+    #monitor= eDP-1, 1920x1080@60.00000, 0x0, 1
+    monitor = monitor.${profile};
   };
 }
