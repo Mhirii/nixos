6a8473b57dc9c21068cbcb42e655dab4b20eac20
Mhirii <ahmedmhiri218@gmail.com>
2024-08-31 02:28:02 +0100

    offload nvidia gpu

--- hosts/laptop/nvidia.nix
+++ hosts/laptop/nvidia.nix
@@ -2,6 +2,10 @@
 {
   hardware.nvidia = {
     prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
       # sync.enable = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:2:0:0";
