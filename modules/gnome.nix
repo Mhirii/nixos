c58ec9cd0e2182ea56c20c330f14b2bf23991ccb
Mhirii <ahmedmhiri218@gmail.com>
2024-09-04 18:24:59 +0100

    flake update

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,4 +1,6 @@
 { ... }: {
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = false;
+  services.gnome.core-shell.enable = true;
+  services.gnome.gnome-keyring.enable = true;
 }
