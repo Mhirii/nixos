a84810d579690478f71a999ef941fc5fead1ffcc
Mhirii <ahmedmhiri218@gmail.com>
2024-09-07 19:12:57 +0100

    battery kernel module

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -35,13 +35,13 @@
 
   # environment.sessionVariables.VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
 
-  # boot = {
-  #   kernelModules = ["acpi_call"];
-  #   extraModulePackages = with config.boot.kernelPackages;
-  #     [
-  #       acpi_call
-  #       cpupower
-  #     ]
-  #     ++ [pkgs.cpupower-gui];
-  # };
+  boot = {
+    kernelModules = [ "acpi_call" ];
+    extraModulePackages = with config.boot.kernelPackages;
+      [
+        acpi_call
+        cpupower
+      ]
+      ++ [ pkgs.cpupower-gui ];
+  };
 }
