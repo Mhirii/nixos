63b19478730a09b008691dfa18e899df85989293
Mhirii <ahmedmhiri218@gmail.com>
2024-09-07 19:13:03 +0100

    disable gnome

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,5 +1,5 @@
 { ... }: {
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = false;
   services.xserver.displayManager.gdm.enable = true;
   services.gnome.core-shell.enable = true;
   services.gnome.gnome-keyring.enable = true;
