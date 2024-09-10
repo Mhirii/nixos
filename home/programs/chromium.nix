1c7df4bd68ff8947f7b42b88514de5992b2a407d
Mhirii <ahmedmhiri218@gmail.com>
2024-09-10 17:26:22 +0100

    chromium config

--- home/programs/chromium.nix
+++ home/programs/chromium.nix
@@ -1,3 +1,11 @@
 {
-  programs.chromium.enable = true;
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      { id = "fmkadmapgofadopljbjfkapdkoienihi"; } # React Developer Tools 
+      { id = "pgamkpjkbfldnmemhcbekimfdnjcgkco"; } # Tailwind CSS Devtools
+      { id = "fcombecpigkkfcbfaeikoeegkmkjfbfm"; } # React Performance Devtool
+      { id = "lmhkpmbekcpmknklioeibfkpmmfibljd"; } # Redux DevTools
+    ];
+  };
 }
