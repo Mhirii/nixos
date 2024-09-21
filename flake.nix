28f3e43525743f234fbdf79ac166a2ecf5592cbc
Mhirii <ahmedmhiri218@gmail.com>
2024-09-21 23:33:04 +0100

    disable offload

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
         config.allowUnfree = true;
       };
       lib = nixpkgs.lib;
-      prefs = { nvidiaOffload = true; };
+      prefs = { nvidiaOffload = false; };
       commonModules = profile: [
         (import ./hosts/${profile})
         inputs.spicetify-nix.nixosModules.default
