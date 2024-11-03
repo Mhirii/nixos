8af43b0318fd5508476a98bbcd244d559b187d75
Mhirii <ahmedmhiri218@gmail.com>
2024-11-04 00:00:30 +0100

    139 current  2024-11-04 00:00:27  24.05.20241012.a3f9ad6  6.6.56                          disableGpu prime proprietary

--- rebuild.sh
+++ rebuild.sh
@@ -18,7 +18,18 @@ git diff -U0 '.*nix'
 
 echo "Nixos Rebuilding"
 trap "echo 'Script interrupted'; exit 1" INT
-sudo nixos-rebuild switch --flake $HOME/flake#laptop &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+sudo nixos-rebuild switch --flake $HOME/flake#laptop 2>&1 | tee nixos-switch.log
+if [ $? -eq 0 ]; then
+  rebuild_success=true
+else
+  cat nixos-switch.log | grep --color error
+  exit 1
+fi
+
+if [ "$rebuild_success" = false ]; then
+  echo "Nixos rebuild failed"
+  exit 1
+fi
 
 current=$(nixos-rebuild list-generations | grep current)
 
