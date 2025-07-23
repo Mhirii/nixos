d2848809a853c32b0f12da42429229072b734699
Mhirii <ahmedmhiri218@gmail.com>
2025-07-23 20:07:13 +0100

    177 current  2025-07-23 20:07:11  24.11.20250506.1d3aeb5  6.6.89                          oss passthrough prime proprietary

--- modules/hardware/network.nix
+++ modules/hardware/network.nix
@@ -5,9 +5,15 @@
     networkmanager.enable = true;
     nameservers = ["1.1.1.1"];
     firewall = {
-      enable = false;
+      enable = true;
       allowedTCPPorts = [22 80 443 59010 59011];
       allowedUDPPorts = [59010 59011];
+      extraCommands = ''
+        iptables -I INPUT 1 -i docker0 -p tcp -d 172.17.0.1 -j ACCEPT
+        iptables -I INPUT 2 -i docker0 -p udp -d 172.17.0.1 -j ACCEPT
+        iptables -I INPUT 3 -s 172.16.0.0/12 -p tcp -d 172.17.0.1 -j ACCEPT
+        iptables -I INPUT 4 -s 172.16.0.0/12 -p udp -d 172.17.0.1 -j ACCEPT
+      '';
       # allowedUDPPortRanges = [
       # { from = 4000; to = 4007; }
       # { from = 8000; to = 8010; }
