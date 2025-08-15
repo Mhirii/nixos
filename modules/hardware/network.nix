1cb792395e8060b21044459e387a549bb9f50c73
Mhirii <ahmedmhiri218@gmail.com>
2025-08-16 00:41:11 +0100

    183 current  2025-08-16 00:41:09  24.11.20250630.50ab793  6.6.94                          *

--- modules/hardware/network.nix
+++ modules/hardware/network.nix
@@ -5,10 +5,10 @@
     networkmanager.enable = true;
     nameservers = ["1.1.1.1"];
     firewall = {
-      enable = false;
-      allowedTCPPorts = [22 80 443 59010 59011];
-      allowedUDPPorts = [59010 59011];
-      trustedInterfaces = ["docker0"];
+      enable = true;
+      allowedTCPPorts = [22 80 443 3000 8080 8081 8082 8083 8443 59010 59011];
+      allowedUDPPorts = [8080 8081 8082 8083 59010 59011];
+      trustedInterfaces = ["docker0" "bridge"];
       extraCommands = ''
         iptables -I INPUT 1 -i docker0 -p tcp -d 172.17.0.1 -j ACCEPT
         iptables -I INPUT 2 -i docker0 -p udp -d 172.17.0.1 -j ACCEPT
