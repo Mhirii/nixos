eafe7e5e8fcd329eac707af9be31fa27a56bab4a
Mhirii <ahmedmhiri218@gmail.com>
2025-08-16 00:06:16 +0100

    181 current  2025-08-16 00:06:14  24.11.20250630.50ab793  6.6.94                          *

--- modules/hardware/network.nix
+++ modules/hardware/network.nix
@@ -5,7 +5,7 @@
     networkmanager.enable = true;
     nameservers = ["1.1.1.1"];
     firewall = {
-      enable = true;
+      enable = false;
       allowedTCPPorts = [22 80 443 59010 59011];
       allowedUDPPorts = [59010 59011];
       trustedInterfaces = ["docker0"];
